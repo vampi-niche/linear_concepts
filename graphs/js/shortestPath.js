d56b009a53048ff31e3f54350a8d0d58a0c212db
vampi-niche <129995695+vampi-niche@users.noreply.github.com>
2023-10-18 13:40:09 -0700

    Create shortestPath.js

---
+++ graphs/js/shortestPath.js
@@ -0,0 +1,57 @@
+const edges = [
+    ['w','x'],
+    ['x','y'],
+    ['z','y'],
+    ['z','v'],
+    ['w','v'],
+
+]
+
+const buildGraph = (edges) => {
+
+    const graph = {}
+
+    for (let edge of edges){
+
+        const [a,b] = edge
+
+        if(!(a in graph)) graph[a] = []
+        if(!(b in graph)) graph[b] = []
+
+        graph[a].push(b)
+        graph[b].push(a)
+
+
+    }
+    return graph
+}
+
+
+const shortestPath = (edges, src, dest) => {
+
+    const graph = buildGraph(edges)
+    const queue = [[src, 0]]
+    
+    const visited = new Set()
+    visited.add(src)
+
+    while (queue.length > 0)
+    {
+        const [curr, dist] = queue.shift()
+
+        if(curr === dest) return dist
+
+        for(let neighbor of graph[curr]){
+            if(!(visited.has(neighbor)))
+            queue.push([neighbor, dist + 1])
+        }
+    }
+
+    return -1
+    //const result = countPath(graph, src, dest, visited)
+
+
+    console.log(result)
+}
+
+console.log(shortestPath(edges, 'w', 'z'))
