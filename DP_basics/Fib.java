a743feb10312e0ad48a2c13d73303901d5719451
vampi-niche <vampi.niche@gmail.com>
2023-10-30 13:50:09 -0700

    fib

--- DP_basics/Fib.java
+++ DP_basics/Fib.java
@@ -1,8 +1,34 @@
 package DP_basics;
 
+import java.util.HashMap;
+
 public class Fib{
     public static void main(String[] args) {
-        System.out.println("test");
         
+        System.out.print(fib(64));
+
+    }
+
+    public static int fib(int n){
+
+
+        return  fib(n, new HashMap<>());
+
+    }
+
+    public static int fib (int n, HashMap<Integer, Integer> memo){
+
+        if(n == 0 || n == 1) return n;
+
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int hold = fib(n-1, memo) + fib(n-2, memo);
+
+        memo.put(n, hold);
+
+        return hold;
+
     }
 }
